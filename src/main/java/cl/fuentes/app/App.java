fda0da9855b0274a650e388975374b7b3864e2ef
Alvaro Fuentes <alvarofue@gmail.com>
2018-07-21 12:15:25 -0400

    Actualiza main

--- src/main/java/cl/fuentes/app/App.java
+++ src/main/java/cl/fuentes/app/App.java
@@ -5,8 +5,10 @@ public class App
     public static void main( String[] args ) throws Exception
     {
 		// Aquí el código
+    	System.out.println("--------------------------------------------");
     	System.out.println("Hola Mundo!!!");
     	System.in.read();
+    	System.out.println("--------------------------------------------");
     }
 
 }
